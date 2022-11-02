1309f4c20b564e8ed733e4021ce537747248d5c4
N0E-P <pecci.noe@gmail.com>
2022-11-02 08:52:27 +0100

    All the PDFs

---
+++ To-do/tp4/Exe3.java
@@ -0,0 +1,15 @@
+package tp4;
+
+public class Exe3 {
+	// Exercice 3 : Miroir
+
+	// TODO
+
+	public static void main(String[] args) {
+		// Un exemple de tableau
+		int[] tab = { 10, 20, 30, 40, 50 };
+
+		// TODO
+	}
+
+}
---
+++ To-do/tp4/Exe4.java
@@ -0,0 +1,31 @@
+package tp4;
+
+public class Exe4 {
+	// Exercice 4 : Parcours de tableau
+
+	// TODO
+
+	public static void main(String[] args) {
+		// Exemple de tableaux
+		int[] tabVals = { 2, 5, 6, 1, 9, 8, 3, 4, 5, 7 };
+		int[] tabDepls = { -2, 3, -1, 4 };
+		int posInit = 4;
+
+		// Le résultat attendu
+		int[] tabExpected = { 6, 8, 9, 5 };
+
+		// Exécuter la fonction que vous avez définie
+		// TODO
+
+		// Affichage du tableau attendu
+		System.out.print("Expected result:");
+		for (int i = 0; i < tabExpected.length; i++) {
+			System.out.print(" " + tabExpected[i]);
+		}
+		System.out.println();
+
+		// Affichage du résultat obtenu, et comparer
+		// TODO
+	}
+
+}
